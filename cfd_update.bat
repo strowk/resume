10939175cd62021e9c52438bfcc0e2bd41f23607
str.write@gmail.com <str.write@gmail.com>
2022-08-13 22:22:31 +0200

    rm mistakenly commited file

--- cfd_update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-sc stop cloudflared >nul 2>&1
-rename "C:\progs\bin\cloudflared.exe" cloudflared.exe.old
-rename "C:\progs\bin\cloudflared.exe.new" cloudflared.exe
-del "C:\progs\bin\cloudflared.exe.old"
-sc start cloudflared >nul 2>&1
-del cfd_update.bat
